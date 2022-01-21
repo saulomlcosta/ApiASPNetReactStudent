ebc6d9d337ac8138f21aaed3b8f7e8ea27b7efe7
Saulo Costa <79532045+saulomlcosta@users.noreply.github.com>
2022-01-21 09:03:17 -0300

    Adding the first page and consuming the API

--- ApiASPNET/ApiASPNET/Startup.cs
+++ ApiASPNET/ApiASPNET/Startup.cs
@@ -44,6 +44,13 @@ namespace ApiASPNET
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ApiASPNET v1"));
             }
 
+            app.UseCors(options =>
+            {
+                options.WithOrigins("http://localhost:3000");
+                options.AllowAnyMethod();
+                options.AllowAnyHeader();
+            });
+
             app.UseHttpsRedirection();
 
             app.UseRouting();
--- studentsreact/src/App.js
+++ studentsreact/src/App.js
@@ -1,11 +1,68 @@
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import { Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+import logoCadastro from './assets/cadastro.png';
+
+
 function App() {
+  const baseUrl = "https://localhost:44354/api/alunos";
+
+  const [data, setData] = useState([]);
+
+  const pedidosGet = async () => {
+    await axios.get(baseUrl)
+      .then((response) => {
+        setData(response.data);
+      }).catch((error) => {
+        console.log(error);
+      })
+  }
+
+  useEffect(() => {
+    pedidosGet();
+  })
+
+  console.log('Data!', data)
   return (
     <div className="App">
-      <header className="App-header">       
-        <h1>Heeloooo</h1>
+      <br />
+      <h3>Cadastro de Alunos</h3>
+      <header>
+        <div style={{"height": "100px"}}>
+          <img className="mh-100" style={{"width": "100px", "height": "200px"}} src={logoCadastro} alt="Cadastro" />
+        </div>
+        <div style={{"height": "100px"}}>
+          <button className="mh-100 mt-2 ml-2 btn btn-success">Incluir Novo Aluno</button>
+        </div>               
       </header>
+      <table className="table table-bordered">
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Idade</th>
+            <th>Operação</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(aluno=>(
+            <tr key={aluno.id}>
+              <td>{aluno.id}</td>
+              <td>{aluno.nome}</td>
+              <td>{aluno.email}</td>           
+              <td>{aluno.idade}</td>
+              <td>
+                <button className="btn btn-primary">Editar</button> {" "}
+                <button className="btn btn-danger">Excluir</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
